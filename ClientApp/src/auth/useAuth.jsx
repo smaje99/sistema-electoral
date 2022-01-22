5d2201825a01b17caa1f736149e58bb8cc77e2f0
Sergio Majé <smajefranco@gmail.com>
2022-01-22 11:58:13 -0500

    ✏️ Adding a missing funtion on useAuth
    Parentheses were missing from the declarative function of the useAuth hook

--- ClientApp/src/auth/useAuth.jsx
+++ ClientApp/src/auth/useAuth.jsx
@@ -2,6 +2,6 @@ import { useContext } from 'react';
 
 import { AuthContext } from './AuthProvider';
 
-const useAuth = useContext(AuthContext);
+const useAuth = () => useContext(AuthContext);
 
 export default useAuth;
