5df7025fcafe1f105d815906d32797e6b03ee6dc
Sergio Majé <smajefranco@gmail.com>
2022-01-22 11:54:18 -0500

    🚧 Login page
    Login page component and route

---
+++ ClientApp/src/pages/Login/Login.jsx
@@ -0,0 +1,5 @@
+const Login = () => (
+    <h1>Inicio de Sesión</h1>
+)
+
+export default Login;
--- ClientApp/src/routers/AppRouter.jsx
+++ ClientApp/src/routers/AppRouter.jsx
@@ -6,11 +6,14 @@ import Layout from '@Components/Layout';
 
 import Dashboard from '@Pages/Dashboard';
 import Home from '@Pages/Home';
+import Login from '@Pages/Login';
 import NotFound from '@Pages/NotFound';
 
 const AppRouter = () => (
     <Routes>
         <Route path="/" element={<Home />} >
+            <Route path="login" element={<Login />} />
+
             <Route element={<PrivateRoutes />}>
                 <Route path="dashboard" element={<Layout />}>
                     <Route index element={<Dashboard />} />
