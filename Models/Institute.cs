e4f51ed7bead3b12f62d2106fb47529ea86d6a74
Sergio Majé <smajefranco@gmail.com>
2022-01-25 11:29:35 -0500

    🚧 Institute model

---
+++ Models/Institute.cs
@@ -0,0 +1,13 @@
+namespace SistemaElectoral.Models
+{
+    public class Institute
+    {
+        public int id { get; set; }
+        public string nit { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string city { get; set; }
+    }
+}
