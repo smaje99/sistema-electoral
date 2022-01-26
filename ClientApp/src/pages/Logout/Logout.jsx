adfd18c731def8db2fb8d4f65549243823c1aab1
Sergio Majé <smajefranco@gmail.com>
2022-01-26 12:35:20 -0500

    🚧 Logout page
    Logout page, component and route

---
+++ ClientApp/src/pages/Logout/Logout.jsx
@@ -0,0 +1,16 @@
+import { Helmet } from 'react-helmet';
+
+const Logout = () => {
+
+    return (
+        <>
+        <Helmet>
+            <title>Crear cuenta</title>
+        </Helmet>
+
+        <h1>Logout</h1>
+        </>
+    )
+}
+
+export default Logout;
