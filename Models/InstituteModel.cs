cee6f54d5fe0feb7d005be0c61319a4a32832756
Sergio Majé <smajefranco@gmail.com>
2022-02-08 22:47:43 -0500

    ⚰️ Remove all dotnet environment

--- Models/InstituteModel.cs
+++
@@ -1,7 +0,0 @@
-namespace SistemaElectoral.Models;
-
-public class Institute
-{
-    public int IdInstitute { get; set; }
-    public string Name { get; set; } = "";
-}
--- Models/RoleModel.cs
+++
@@ -1,7 +0,0 @@
-namespace SistemaElectoral.Models;
-
-public class Role
-{
-    public int IdRole { get; set; }
-    public string Name { get; set; } = "";
-}
