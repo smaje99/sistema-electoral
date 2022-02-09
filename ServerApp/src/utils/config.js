36545f86487c8b2cc107aa8ee8f1297b1ab6b04f
Sergio Majé <smajefranco@gmail.com>
2022-02-08 23:39:49 -0500

    🔧 Add env attr config

--- ServerApp/src/utils/config.js
+++ ServerApp/src/utils/config.js
@@ -2,5 +2,6 @@ require('dotenv').config();
 
 /* This is the configuration for the Node.js server. */
 module.exports = {
-    Port: process.env.PORT || 8080
+    Port: process.env.PORT || 8080,
+    Dev: process.env.NODE_ENV !== 'production',
 }
