d373928b3dd9b259a61fe4870744b2a7fdd54334
Sergio Majé <smajefranco@gmail.com>
2022-02-11 22:09:30 -0500

    🔧 Add database credentials

--- ServerApp/src/utils/config.js
+++ ServerApp/src/utils/config.js
@@ -4,4 +4,11 @@ require('dotenv').config();
 module.exports = {
     Port: process.env.PORT || 8080,
     Dev: process.env.NODE_ENV !== 'production',
+    ConnectionOptions: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_UID,
+        password: process.env.DB_PWD,
+        database: process.env.DB_DB
+    }
 }
