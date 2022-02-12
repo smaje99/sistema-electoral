42eeee865d307252528ea2ce76edbdadbd8d4be8
Sergio Majé <smajefranco@gmail.com>
2022-02-12 17:45:31 -0500

    🗃️ Function to check email and pwd of an user

---
+++ Database/Procedures/fnCheckPassword.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION `election`.`fnCheckPassword`(
+    _email varchar(50),
+    _password varchar(50)
+) RETURNS tinyint(1)
+    READS SQL DATA
+BEGIN
+    return exists(
+        select idUser
+        from user
+        where email = _email and password = _password
+    );
+END
