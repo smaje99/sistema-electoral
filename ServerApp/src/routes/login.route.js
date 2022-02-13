84da92a22aabe43be99a443ad9ee8754b49f6e3d
Sergio Majé <smajefranco@gmail.com>
2022-02-13 18:26:09 -0500

    🚧 Add validation handler to login route

--- ServerApp/src/routes/login.route.js
+++ ServerApp/src/routes/login.route.js
@@ -1,19 +1,21 @@
-const { Router } = require('express');
-
-const { LoginSessionError } = require('../utils/errors');
+const router = require('express').Router();
 
 const service = require('../services/userSession.service');
 
-const router = Router();
+const { LoginSessionError } = require('../utils/errors');
+const schema = require('../utils/schemas/login.schema');
+const validation = require('../middleware/validation.handler');
 
-router.post('/', async (req, res) => {
+router.post('/', validation(schema), async (req, res, next) => {
+    const { email, password } = req.body;
+
     try {
-        const { email, password } = req.body;
         const data = await service.login(email, password);
         res.status(200).json(data);
     } catch (error) {
         if (error instanceof LoginSessionError)
             res.status(error.statusCode).json({ error: error.message })
+        else next(error);
     }
 })
 
