936da5b8a8907496d1f6e9465cadbcec9edfbbab
Sergio Majé <smajefranco@gmail.com>
2022-02-14 18:24:42 -0500

    🔧 Toast configuration

---
+++ client/src/utils/config.js
@@ -0,0 +1,10 @@
+import { toast } from 'react-toastify';
+
+const config = {
+    toast: {
+        position: toast.POSITION.BOTTOM_RIGHT,
+        className: 'toast'
+    }
+}
+
+export default config;
