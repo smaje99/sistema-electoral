af201c3db82379c3e15752cf17174dc63e5c7fe7
Sergio Majé <smajefranco@gmail.com>
2022-02-14 12:15:29 -0500

    🚧 Wizard component

--- client/src/components/Wizard/Component.jsx
+++ client/src/components/Wizard/Component.jsx
@@ -3,19 +3,23 @@ import useWizard from './useWizard';
 import './style.css';
 
 const Wizard = () => {
-    const wizard = useWizard();
+    const { currentIndex, items } = useWizard();
+
+    const wizardDotClass = index => (
+        (index == currentIndex ? 'wizard--active ' : '').concat('wizard__dot')
+    )
 
     return (
         <div className="wizard">
-            {Array.from(wizard.items.entries()).map(([index, value]) => (
-                <div className="wizard__step">
+            {Array.from(items.entries()).map(([index, value]) => (
+                <div className="wizard__step" key={index}>
                     <span className="wizard__title">{value}</span>
-                    <div className="wizard__dot wizard--active">
-                        <div class="wizard__connector"></div>
+                    <div className={wizardDotClass(index)}>;
+                        <div className="wizard__connector"></div>
                         <div className="wizard__bullet">
                             <span className="wizard__bullet--number">{index + 1}</span>
                         </div>
-                        <div class="wizard__connector"></div>
+                        <div className="wizard__connector"></div>
                     </div>
                 </div>
             ))}
