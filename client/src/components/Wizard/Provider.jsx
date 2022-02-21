84de87e247b2a05a69724a38693a4a81ffc0d9eb
Sergio Majé <smajefranco@gmail.com>
2022-02-21 16:09:03 -0500

    🎨 Update navigation methods on wizard

--- client/src/components/Wizard/Provider.jsx
+++ client/src/components/Wizard/Provider.jsx
@@ -1,23 +1,21 @@
 import { createContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 export const WizardContext = createContext();
 
-export const WizardProvider = ({ items, children }) => {
-    const navigate = useNavigate();
+export const WizardProvider = ({ items, handleBack, handleNext, children }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    const back = (toLocation) => {
+    const back = () => {
         if (currentIndex > 0) {
-            setCurrentIndex(currentIndex - 1);
-            navigate(toLocation);
+            setCurrentIndex(prevIndex => prevIndex - 1);
+            handleBack(currentIndex);
         }
     }
 
-    const next = (toLocation) => {
+    const next = () => {
         if (currentIndex + 1 < items.length) {
-            setCurrentIndex(currentIndex + 1);
-            navigate(toLocation);
+            setCurrentIndex(prevIndex => prevIndex + 1);
+            handleNext(currentIndex);
         }
     }
 
