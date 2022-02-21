869f84b291a6536b23137300708d3f7ac23d1ef8
Sergio Majé <smajefranco@gmail.com>
2022-02-21 16:13:42 -0500

    🧐 Signup resolver

--- client/src/resolvers/signup.resolver.js
+++ client/src/resolvers/signup.resolver.js
@@ -9,19 +9,26 @@ const gender = yup.number().oneOf([0, 1], 'Debe seleccionar algún genero');
 const phone = yup.string();
 const address = yup.string();
 
-export const personalResolver = yupResolver(yup.object().shape({
+const personalSchema = yup.object().shape({
     dni: dni.required('Debe ingresar un DNI'),
     name: name.required('Debe ingresar un nombre'),
     email: email.required('Debe ingresar un correo electrónico'),
     password: password.required('Debe ingresar una contraseña'),
     gender: gender.required('Debe ingresar un genero'),
     phone: phone.required('Debe ingresar un número de teléfono')
-}))
+})
 
-export const instituteResolver = yupResolver(yup.object().shape({
+const instituteSchema = yup.object().shape({
     name: name.required('Debe ingresar un nombre'),
     nit: dni.required('Debe ingresar un NIT'),
     email: email.required('Debe ingresar un correo electrónico'),
     address: address.required('Debe ingresar una dirección'),
     phone: phone.required('Debe ingresar un teléfono'),
+})
+
+const signupResolver = yupResolver(yup.object().shape({
+    personal: personalSchema.required('Debe de ingresar los datos personales'),
+    institute: instituteSchema.required('Debe de ingresar los datos institucionales')
 }))
+
+export default signupResolver;
