858750066b767791a98a3f0c677828e18c4c1c90
Sergio Majé <smajefranco@gmail.com>
2022-02-23 12:16:06 -0500

    🔥 Remove the Insert of a institute in the SP

--- Database/Procedures/spAdmin_Insert.sql
+++ Database/Procedures/spAdmin_Insert.sql
@@ -1,37 +1,26 @@
 use election;
 
 create procedure spAdmin_Insert(
-    _emailUser varchar(50),
+    _email varchar(50),
     `_password` varchar(50),
     _dni varchar(25),
-    _nameUser varchar(150),
+    _name varchar(150),
     _gender bool,
-    _phoneUser varchar(15),
-    _nit varchar(25),
-    _nameInstitute varchar(150),
-    _address varchar(50),
-    _emailInstitute varchar(50),
-    _phoneInstitute varchar(15)
+    _phone varchar(15),
+    _idInstitute integer unsigned
 )
 begin
-    -- insert into to institute
-    insert
-        into institute(nit, `name`, `address`, email, phone)
-        values (_nit, _nameInstitute, _address, _emailInstitute, _phoneInstitute);
-
-    set @idInstitute := LAST_INSERT_ID();
-
     -- insert into to personal data
     insert
         into personaldata(dni, `name`, gender, phone)
-        values (_dni, _nameUser, _gender, _phoneUser);
+        values (_dni, _name, _gender, _phone);
 
     set @idPersonalData := LAST_INSERT_ID();
 
     -- insert into to user
     insert
         into `user`(email, `password`, personaldata, institute, `role`)
-        values (_emailUser, `_password`, @idPersonalData, @idInstitute, 1);
+        values (_email, `_password`, @idPersonalData, _idInstitute, 1);
 
     -- returns the user's session information
     call spUserSession_Login(_email, `_password`);
