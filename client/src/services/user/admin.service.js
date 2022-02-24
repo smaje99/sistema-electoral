903aaf75c87708a0803d9d15b5920fbd958f0711
Sergio Majé <smajefranco@gmail.com>
2022-02-24 10:21:55 -0500

    🎨 Add institute parameter in the admin service

--- client/src/services/user/admin.service.js
+++ client/src/services/user/admin.service.js
@@ -3,11 +3,11 @@ const API = process.env.API;
 const getURLAdmin = `${API}/user/admin`;
 
 class AdminService {
-    async create(userData) {
+    async create(userData, idInstitute) {
         try {
             const res = await fetch(getURLAdmin, {
                 method: 'PUT',
-                body: JSON.stringify(userData),
+                body: JSON.stringify({ ...userData, idInstitute }),
                 headers: { 'Content-type': 'application/json' }
             })
             const sessionData = await res.json();
