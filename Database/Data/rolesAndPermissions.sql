34c7f5b50c8e82b44a32e5dead24336ad9b9945d
Sergio Majé <smajefranco@gmail.com>
2022-02-25 12:03:43 -0500

    📸 Add data of the menu and the private routes

--- Database/Data/rolesAndPermissions.sql
+++ Database/Data/rolesAndPermissions.sql
@@ -6,3 +6,13 @@ insert into `action` (`name`)
         ("create"),  -- 2
         ("edit"),  -- 3
         ("vote");  -- 4
+
+insert into menu (item, `route`)
+    values
+        ("Información Personal", "/dashboard/info"),  -- 1
+        ("Usuarios", "/dashboard/users"),  -- 2
+        ("Candidatos", "/dashboard/candidates"),  -- 3
+        ("Votaciones", "/dashboard/vote"),  -- 4
+        ("Elecciones", "/dashboard/elections"),  -- 5
+        ("Convocatorias", "/dashboard/announcement"),  -- 6
+        ("Roles, Cargos y Permisos", "/dashboard/roles");  -- 7
