239069abfe7270b2912849ab671c63baafb6f88c
Sergio Majé <smajefranco@gmail.com>
2022-02-25 12:06:53 -0500

    📸 Add permissions data related to menu and actions

--- Database/Data/rolesAndPermissions.sql
+++ Database/Data/rolesAndPermissions.sql
@@ -16,3 +16,23 @@ insert into menu (item, `route`)
         ("Elecciones", "/dashboard/elections"),  -- 5
         ("Convocatorias", "/dashboard/announcement"),  -- 6
         ("Roles, Cargos y Permisos", "/dashboard/roles");  -- 7
+
+insert into permission (menu, `action`)
+    values
+        (1, 1),  -- 1
+        (1, 3),  -- 2
+        (2, 1),  -- 3
+        (2, 2),  -- 4
+        (2, 3),  -- 5
+        (3, 1),  -- 6
+        (3, 2),  -- 7
+        (3, 3),  -- 8
+        (4, 1),  -- 9
+        (4, 4),  -- 10
+        (5, 1),  -- 11
+        (6, 1),  -- 12
+        (6, 2),  -- 13
+        (6, 3),  -- 14
+        (7, 1),  -- 15
+        (7, 2),  -- 16
+        (7, 3);  -- 17
