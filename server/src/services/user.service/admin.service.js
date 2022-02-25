94fef8157991ad5cdc9036da3432c7e783b02ec5
Sergio Majé <smajefranco@gmail.com>
2022-02-24 23:27:51 -0500

    ✏️ Change parameter name

--- server/src/services/user.service/admin.service.js
+++ server/src/services/user.service/admin.service.js
@@ -8,12 +8,12 @@ const {
 
 class AdminService {
     async createAccount(accountData) {
-        const {dni, name, email, password, gender, phone} = accountData;
+        const {dni, name, email, password, gender, phone, idInstitute} = accountData;
 
         try {
-            const [[[sessionData]]] = query(
+            const [[[sessionData]]] = await query(
                 'CALL spAdmin_Insert(?, ?, ?, ?, ?, ?, ?)',
-                [dni, name, email, password, gender, phone]
+                [dni, name, email, password, gender, phone, idInstitute]
             )
 
             const user = new User({
