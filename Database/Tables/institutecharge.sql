7de3f56110db34ea69e9c692bfe3752b01de1fdf
Sergio Majé <smajefranco@gmail.com>
2022-03-04 19:07:10 -0500

    🚧 Create institutecharge table

---
+++ Database/Tables/institutecharge.sql
@@ -0,0 +1,7 @@
+create table if not exists election.institutecharge (
+    idInstituteCharge integer unsigned unique primary key not null auto_increment unique,
+    institute integer unsigned not null,
+    charge integer unsigned not null,
+    foreign key (institute) references institute (idInstitute),
+    foreign key (charge) references charge (idCharge)
+);
