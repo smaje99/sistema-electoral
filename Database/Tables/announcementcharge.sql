7e7b9b6825ff2170b38e4dd8308ca9e911d53110
Sergio Majé <smajefranco@gmail.com>
2022-03-04 19:30:33 -0500

    🎨 Replace charge attr with institutecharge attr

--- Database/Tables/announcementcharge.sql
+++ Database/Tables/announcementcharge.sql
@@ -1,7 +1,7 @@
 create table if not exists election.announcementcharge (
-	`idAnnouncementCharge` int unsigned PRIMARY KEY NOT NULL AUTO_INCREMENT UNIQUE,
-  	`announcement` int unsigned NOT NULL,
-  	`charge` int unsigned NOT NULL,
-  	FOREIGN KEY (`announcement`) REFERENCES `announcement` (`idAnnouncement`),
-  	FOREIGN KEY (`charge`) REFERENCES `charge` (`IdCharge`)
+	idAnnouncementCharge integer unsigned PRIMARY KEY NOT NULL AUTO_INCREMENT UNIQUE,
+  	announcement integer unsigned NOT NULL,
+  	institutecharge integer unsigned NOT NULL,
+  	FOREIGN KEY (announcement) REFERENCES announcement (idAnnouncement),
+  	FOREIGN KEY (institutecharge) REFERENCES institutecharge (idInstituteCharge)
 );
