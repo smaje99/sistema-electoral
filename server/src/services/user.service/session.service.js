6e4e9199dce6635f2fdab9656f753bff22c0582a
Sergio Majé <smajefranco@gmail.com>
2022-03-05 11:46:09 -0500

    👔 Add permissions to session data
    CHANGE
    * Better error handling within the service
    
    ADD
    * creation and management of permissions

--- server/src/services/user.service/session.service.js
+++ server/src/services/user.service/session.service.js
@@ -4,21 +4,26 @@ const { query } = require('../../utils/database');
 const { HTTPError } = require('../../utils/errors');
 
 const {
-    Institute, PersonalData, Role, User
+    Institute, Permission, PersonalData, Role, User
 } = require('../../models');
 
 class UserSessionService {
     async login(email, password) {
         try {
-            const [[[data]]] = await query(`CALL spUserSession_Login(?, ?)`, [email, password]);
+            const [[[userData]]] = await query(
+                'CALL spUserSession_Login(?, ?)',
+                [ email, password ]
+            );
 
-            if (data?.isAuth == 0)
+            if (userData?.isAuth == 0)
                 throw new HTTPError("Credenciales Invalidas", 203);
 
-            const user = this.#createUser(data);
-            return user;
+            const user = this.#createUser(userData);
+            const permissions = await this.#createPermissions(userData);
+            const sessionData = { ...user, permissions };
+            return sessionData;
         } catch (error) {
-            throw boom.badImplementation(error);
+            throw boom.badImplementation(error, error.message);
         }
     }
 
@@ -37,6 +42,27 @@ class UserSessionService {
             role: new Role({ idRole: data.idInstituteRole, name: data.role })
         });
         return user;
+    }
+
+    async #createPermissions({ idUser, idInstituteRole }) {
+        const [[permissionsData]] = await query(
+            'CALL spPermissions_Get(?, ?)',
+            [ idUser, idInstituteRole ]
+        );
+
+        let permissions = new Map();
+        permissionsData.forEach(({ menu, route, action, isActive }) => {
+            if (permissions.has(menu)) {
+                const permission = permissions.get(menu);
+                permission.addAction(action);
+            } else {
+                const permission = new Permission({ menu, route, isActive });
+                permission.addAction(action);
+                permissions.set(menu, permission);
+            }
+        });
+        permissions = Array.from(permissions.values());
+        return permissions;
     }
 }
 
