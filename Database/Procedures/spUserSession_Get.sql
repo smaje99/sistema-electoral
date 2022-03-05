dc12bc0e6e9799f03e5f9778a20016abba1afa84
Sergio Majé <smajefranco@gmail.com>
2022-03-04 23:32:05 -0500

    ♻️ Add join of role tables

--- Database/Procedures/spUserSession_Get.sql
+++ Database/Procedures/spUserSession_Get.sql
@@ -11,7 +11,7 @@ begin
 		u.isActive,
 		i.idInstitute,
 		i.`name` as institute,
-		r.idRole,
+		ir.idInstituteRole,
 		r.`name` as `role`
 	from
 		`user` as u
@@ -19,8 +19,10 @@ begin
 		on u.personalData = pd.idPersonalData
 	inner join institute as i
 		on u.institute = i.idInstitute
+	inner join instituterole as ir
+		on u.instituterole = ir.idInstituteRole
 	inner join `role` as r
-		on u.role = r.idRole
+		on ir.`role` = r.idRole
 	where
 		u.email = _email;
 end;
