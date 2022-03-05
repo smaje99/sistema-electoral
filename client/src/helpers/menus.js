ceb0a93e8e75f1c6281bfd2a157230f398cc37c5
Sergio Majé <smajefranco@gmail.com>
2022-03-05 16:38:32 -0500

    🏷️ Create menus helper

---
+++ client/src/helpers/menus.js
@@ -0,0 +1,11 @@
+const menus = {
+    info: 'Información Personal',
+    users: 'Usuarios',
+    candidates: 'Candidatos',
+    vote: 'Votaciones',
+    elections: 'Elecciones',
+    announcement: 'Convocatorias',
+    roles: 'Roles, Cargos y Permisos'
+}
+
+export default menus;
